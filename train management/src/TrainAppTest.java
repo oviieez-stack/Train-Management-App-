342ad7ce10f2c9e396bc0b0f3458d8aacbe6e466
oviieez-stack <oviyashri18@gmail.com>
2026-04-10 15:12:24 +0530

    UC18: Implemented linear search to find bogie ID in an unsorted array

--- train management/src/TrainAppTest.java
+++ train management/src/TrainAppTest.java
@@ -1,18 +1,32 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class UC17SortNames {
+public class UC18LinearSearch {
 
     public static void main(String[] args) {
 
-        String[] bogieNames = {
-                "Sleeper", "AC Chair", "First Class", "General", "Luxury"
-        };
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        // Sort using built-in method
-        Arrays.sort(bogieNames);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Bogie ID to search: ");
+        String key = sc.nextLine();
 
-        // Display result
-        System.out.println("Sorted Bogie Names:");
-        System.out.println(Arrays.toString(bogieNames));
+        boolean found = false;
+
+        // Linear Search
+        for (int i = 0; i < bogieIds.length; i++) {
+            if (bogieIds[i].equals(key)) {
+                found = true;
+                break; // stop when found
+            }
+        }
+
+        // Result
+        if (found) {
+            System.out.println("Bogie Found!");
+        } else {
+            System.out.println("Bogie Not Found!");
+        }
+
+        sc.close();
     }
 }
